44e6a91ff2ef2a0173d3fcb3f9d3bc2776932cbb
smsh0722 <smsh0722@gmail.com>
2021-12-02 14:52:15 +0900

    lock init

--- PA3/server.c
+++ PA3/server.c
@@ -13,47 +13,138 @@
 
 const int MAXUSER = 1023;
 const int MINUSER = 0;
-const int MAXID = 5;
-const int MINID = 0;
+const int MAXACTION = 5;
+const int MINACTION = 0;
 const int MAXSEAT = 255;
 const int MINSEAT = 0;
+
+enum actions {
+    TERMINATE = 0,
+    LOGIN,
+    RESERVE,
+    CHECK_RES,
+    CANCEL_RES,
+    LOGOUT,
+};
 
 typedef struct {
     int user;
     int action;
     int data;
 } query;
-
 typedef struct {
     int passcode;
     int seat;
+    char signup;
 } user;
+typedef struct {
+    int* seats;
+    user* users;
+    int connfd;
+    pthread_mutex_t lock_connfd;
+    pthread_mutex_t lock_seat;
+    pthread_mutex_t* lock_users;
+} client_arg;
 
-int* seats;
-user* users;
-
-void* client_handler( void* arg )
+void* client_handler( void* args_void )
 {
+    client_arg* args = ( (client_arg*)args_void );
     int n;
     query* q;
-    int connfd = *( (int*)arg );
+    int connfd;
+    {
+        pthread_mutex_lock( &(args->lock_connfd) );
+        connfd = args->connfd;
+        pthread_mutex_unlock( &(args->lock_connfd) );
+    }
 
     pthread_detach( pthread_self() );
-    free(arg);
     
     printf( "client_handler!\n" );
+    enum actions action;
     q = (query*)malloc( sizeof(query) );
-    while ( ( n = read(connfd, q, sizeof(query) ) ) > 0 ){
+    char logInFlag = 0; // false == 0, true == 1
+    while ( ( n = read( connfd, q, sizeof(query) ) ) > 0 ){
         printf( "user: %d, action: %d, data: %d\n", q->user, q->action, q->data ); // Debug
+        if ( q->user > MAXUSER || q->user < MINUSER ){
+            write( connfd, (int*)-1, sizeof(int) );
+            continue;
+        }
+        if ( q->action > MAXACTION || q->action < MINACTION ){
+            write( connfd, (int*)-1, sizeof(int) );
+            continue;
+        }
+
+        action = (enum actions)(q->action);
+        /* Not logged in */
+        if ( logInFlag == 0 ){
+            if ( action != LOGIN ){
+                write( connfd, (int*)-1, sizeof(int) );
+                continue;
+            }
+
+            if ( pthread_mutex_trylock( &(args->lock_users[q->user]) ) != 0 ){
+                write( connfd, (int*)-1, sizeof(int) );
+                continue;
+            }
+            /* sign up & log in*/
+            if ( args->users[q->user].signup == -1 ){
+                args->users[q->user].passcode = q->data;
+                args->users[q->user].signup = 1;
+                logInFlag = 1;
+                write( connfd, (int*)1, sizeof(int) );
+                continue;
+            }
+            /* log in */
+            else {
+                if ( args->users[q->user].passcode == q->data ){
+                    logInFlag = 1;
+                    write( connfd, (int*)1, sizeof(int) );
+                }
+                else 
+                    write( connfd, (int*)1, sizeof(int) );
+                continue;
+            }
+        }
+        switch (action)
+        {
+            case TERMINATE:
+            {
+                break;
+            }
+            case LOGIN:
+            {
+                /* code */
+                break;
+            }
+            case RESERVE:
+            {
+                break;
+            }
+            case CHECK_RES:
+            {
+                break;
+            }
+            case CANCEL_RES:
+            {
+                break;
+            }
+            case LOGOUT:
+            {
+                break;
+            }
+        }
     }
 
     free(q);
-    close(connfd);
+    close( connfd );
     return NULL;
 }
 
 int main( int argc, char* argv[] )
 {
+    write( 1, (int*)-1, sizeof(int) );
+    return 0;
     if ( argc != 2 ){
         printf( "Usage: %s <port number>\n", argv[0] );
         exit(1);
@@ -82,34 +173,37 @@ int main( int argc, char* argv[] )
         exit(3);
     }
 
-    int* connfdp;
+    int connfd;
     pthread_t tid;
-    seats = (int*)malloc( sizeof(int)*(MAXSEAT + 1) );
-    users = (user*)malloc( sizeof(user)*(MAXUSER + 1 ) );
-    memset( seats, -1, sizeof(int)*(MAXSEAT + 1) );
-    memset( users, -1, sizeof(*users)*(MAXUSER + 1) );
-    /*
-    { // Debug
-        for ( int i = 0; i <= MAXSEAT; i++ )
-            printf( "%d\n", seats[i] );
+    int seats[MAXSEAT+1];
+    user users[MAXUSER+1];
+    memset( seats, -1, sizeof(int)*(MAXSEAT+1) );
+    memset( users, -1, sizeof(user)*(MAXUSER+1) );
+    client_arg* args = malloc( sizeof(client_arg) );
+    {
+        args->seats = seats;
+        args->users = users;
+        pthread_mutex_init( &(args->lock_connfd), NULL );
+        pthread_mutex_init( &(args->lock_seat), NULL );
+        args->lock_users = malloc( sizeof(pthread_mutex_t)*(MAXUSER+1) );
         for ( int i = 0; i <= MAXUSER; i++ )
-            printf( "[%d] passcode: %d, seat: %d\n", i, users[i].passcode, users[i].seat );
-        return 0;
+            pthread_mutex_init( &( args->lock_users[i] ), NULL );
     }
-    */
     while ( 1 ){
-        connfdp = (int*)malloc(sizeof(int));
-        if ( ( *connfdp = accept( listenfd, (struct sockaddr*)&caddr, (socklen_t*)&caddrlen ) ) < 0 ){
+        caddrlen = sizeof(caddr);
+        if ( ( connfd = accept( listenfd, (struct sockaddr *)&caddr, (socklen_t*)&caddrlen ) ) < 0 ){
             printf( "accept() failed.\n" );
-            free(connfdp);
             continue;
         }
 
-        pthread_create( &tid, NULL, client_handler, (void*)connfdp );
+        /* Handle New client */
+        {
+            pthread_mutex_lock( &(args->lock_connfd) );
+            args->connfd = connfd;
+            pthread_mutex_unlock( &(args->lock_connfd) );
+            pthread_create( &tid, NULL, client_handler, (void*)args );
+        }
     }
-    
-    free( seats );
-    free( users );
 
     return 0;
 }
